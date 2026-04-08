0d80ae041fbc7ee2a25e9f0c69a44a70a612ebdd
Rolan Bacalla <rolan.bacalla@gmail.com>
2026-04-08 12:08:38 +0100

    Fix: Restore Tailwind 3 stable for robust styling and build

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Roboto Serif", "serif"],
+        sans: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        lime: {
+          50: '#f7fee7',
+          100: '#ecfccb',
+          200: '#d9f99d',
+          300: '#bef264',
+          400: '#a3e635',
+          500: '#84cc16',
+          600: '#65a30d',
+          700: '#4d7c0f',
+          800: '#3f6212',
+          900: '#365314',
+          950: '#1a2e05',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
